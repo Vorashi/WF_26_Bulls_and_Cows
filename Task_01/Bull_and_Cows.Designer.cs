d984c66f4784281c16abdfa6234c213eee517d9c
matve <matve@DESKTOP-GPMHDR2>
2024-02-29 09:01:11 +0300

    Добавьте файлы проекта.

---
+++ Task_01/Bull_and_Cows.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Task_01
+{
+    partial class Bull_and_Cows
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.LblResult = new System.Windows.Forms.Label();
+            this.lbxMainUser = new System.Windows.Forms.ListBox();
+            this.lbxBics = new System.Windows.Forms.ListBox();
+            this.lbxCorov = new System.Windows.Forms.ListBox();
+            this.btnInput = new System.Windows.Forms.Button();
+            this.lblUserNumber = new System.Windows.Forms.Label();
+            this.lblBics = new System.Windows.Forms.Label();
+            this.lblCorov = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuItemIntoProgramm = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemClose = new System.Windows.Forms.ToolStripMenuItem();
+            this.tbxInput = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnNewGame.Location = new System.Drawing.Point(93, 480);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(375, 43);
+            this.btnNewGame.TabIndex = 0;
+            this.btnNewGame.Text = "Новая Игра";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // LblResult
+            // 
+            this.LblResult.AutoSize = true;
+            this.LblResult.Location = new System.Drawing.Point(16, 507);
+            this.LblResult.Name = "LblResult";
+            this.LblResult.Size = new System.Drawing.Size(0, 16);
+            this.LblResult.TabIndex = 1;
+            // 
+            // lbxMainUser
+            // 
+            this.lbxMainUser.Enabled = false;
+            this.lbxMainUser.FormattingEnabled = true;
+            this.lbxMainUser.ItemHeight = 16;
+            this.lbxMainUser.Location = new System.Drawing.Point(24, 75);
+            this.lbxMainUser.Name = "lbxMainUser";
+            this.lbxMainUser.Size = new System.Drawing.Size(238, 244);
+            this.lbxMainUser.TabIndex = 2;
+            // 
+            // lbxBics
+            // 
+            this.lbxBics.Enabled = false;
+            this.lbxBics.FormattingEnabled = true;
+            this.lbxBics.ItemHeight = 16;
+            this.lbxBics.Location = new System.Drawing.Point(348, 75);
+            this.lbxBics.Name = "lbxBics";
+            this.lbxBics.Size = new System.Drawing.Size(120, 164);
+            this.lbxBics.TabIndex = 3;
+            // 
+            // lbxCorov
+            // 
+            this.lbxCorov.Enabled = false;
+            this.lbxCorov.FormattingEnabled = true;
+            this.lbxCorov.ItemHeight = 16;
+            this.lbxCorov.Location = new System.Drawing.Point(348, 289);
+            this.lbxCorov.Name = "lbxCorov";
+            this.lbxCorov.Size = new System.Drawing.Size(120, 164);
+            this.lbxCorov.TabIndex = 4;
+            // 
+            // btnInput
+            // 
+            this.btnInput.Enabled = false;
+            this.btnInput.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnInput.Location = new System.Drawing.Point(24, 389);
+            this.btnInput.Name = "btnInput";
+            this.btnInput.Size = new System.Drawing.Size(238, 43);
+            this.btnInput.TabIndex = 5;
+            this.btnInput.Text = "Ввод";
+            this.btnInput.UseVisualStyleBackColor = true;
+            this.btnInput.Click += new System.EventHandler(this.btnInput_Click);
+            // 
+            // lblUserNumber
+            // 
+            this.lblUserNumber.AutoSize = true;
+            this.lblUserNumber.Location = new System.Drawing.Point(21, 46);
+            this.lblUserNumber.Name = "lblUserNumber";
+            this.lblUserNumber.Size = new System.Drawing.Size(86, 16);
+            this.lblUserNumber.TabIndex = 6;
+            this.lblUserNumber.Text = "Ваше число:";
+            // 
+            // lblBics
+            // 
+            this.lblBics.AutoSize = true;
+            this.lblBics.Location = new System.Drawing.Point(345, 46);
+            this.lblBics.Name = "lblBics";
+            this.lblBics.Size = new System.Drawing.Size(43, 16);
+            this.lblBics.TabIndex = 7;
+            this.lblBics.Text = "Быки:";
+            // 
+            // lblCorov
+            // 
+            this.lblCorov.AutoSize = true;
+            this.lblCorov.Location = new System.Drawing.Point(345, 261);
+            this.lblCorov.Name = "lblCorov";
+            this.lblCorov.Size = new System.Drawing.Size(59, 16);
+            this.lblCorov.TabIndex = 8;
+            this.lblCorov.Text = "Коровы:";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuItemIntoProgramm,
+            this.menuItemClose});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(510, 28);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuItemIntoProgramm
+            // 
+            this.menuItemIntoProgramm.Name = "menuItemIntoProgramm";
+            this.menuItemIntoProgramm.Size = new System.Drawing.Size(118, 24);
+            this.menuItemIntoProgramm.Text = "О программе";
+            this.menuItemIntoProgramm.Click += new System.EventHandler(this.menuItemIntoProgramm_Click);
+            // 
+            // menuItemClose
+            // 
+            this.menuItemClose.Name = "menuItemClose";
+            this.menuItemClose.Size = new System.Drawing.Size(67, 24);
+            this.menuItemClose.Text = "Выход";
+            this.menuItemClose.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // tbxInput
+            // 
+            this.tbxInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbxInput.Enabled = false;
+            this.tbxInput.Location = new System.Drawing.Point(24, 347);
+            this.tbxInput.Name = "tbxInput";
+            this.tbxInput.Size = new System.Drawing.Size(238, 22);
+            this.tbxInput.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 560);
+            this.Controls.Add(this.tbxInput);
+            this.Controls.Add(this.lblCorov);
+            this.Controls.Add(this.lblBics);
+            this.Controls.Add(this.lblUserNumber);
+            this.Controls.Add(this.btnInput);
+            this.Controls.Add(this.lbxCorov);
+            this.Controls.Add(this.lbxBics);
+            this.Controls.Add(this.lbxMainUser);
+            this.Controls.Add(this.LblResult);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "WF_26 Быки коровы";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Label LblResult;
+        private System.Windows.Forms.ListBox lbxMainUser;
+        private System.Windows.Forms.ListBox lbxBics;
+        private System.Windows.Forms.ListBox lbxCorov;
+        private System.Windows.Forms.Button btnInput;
+        private System.Windows.Forms.Label lblUserNumber;
+        private System.Windows.Forms.Label lblBics;
+        private System.Windows.Forms.Label lblCorov;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuItemIntoProgramm;
+        private System.Windows.Forms.TextBox tbxInput;
+        private System.Windows.Forms.ToolStripMenuItem menuItemClose;
+    }
+}
+
---
+++ Task_01/Bull_and_Cows.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Task_01
+{
+    public partial class Bull_and_Cows : Form
+    {
+        public int[] secretNumber;
+        public Random random = new Random();
+        public Bull_and_Cows()
+        {
+            InitializeComponent();
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            if (tbxInput.Enabled == true)
+            {
+                tbxInput.Enabled = false;
+                lbxBics.Enabled = false;
+                lbxCorov.Enabled = false;
+                lbxMainUser.Enabled = false;
+                btnInput.Enabled = false;
+                tbxInput.Text = "";
+                lbxMainUser.Items.Clear();
+                lbxCorov.Items.Clear();
+                lbxBics.Items.Clear();
+                btnNewGame.Text = "Новая Игра!";
+                lbxMainUser.Items.Clear();
+                lbxBics.Items.Clear();
+                lbxCorov.Items.Clear();
+            }
+            else
+            {
+                tbxInput.Enabled = true;
+                lbxBics.Enabled = true;
+                lbxCorov.Enabled = true;
+                lbxMainUser.Enabled = true;
+                btnInput.Enabled = true;
+                btnNewGame.Text = "Перезапуск";
+            }
+            secretNumber = GenerateSecretNumber();
+            LblResult.Text = string.Join("", secretNumber);
+        }
+
+        private int[] GenerateSecretNumber()
+        {
+            List<int> numbers = Enumerable.Range(0, 10).ToList();
+            numbers = numbers.OrderBy(x => random.Next()).ToList();
+            return numbers.Take(4).ToArray();
+        }
+
+        private void menuItemIntoProgramm_Click(object sender, EventArgs e)
+        {
+            string textReader = "Игра «Быки и коровы»\r\nКомпьютер загадывает четырехзначное число, цифры в котором не \r\nповторяются. Ваша задача отгадать его. Введите свой вариант и нажмите \r\n\"ВВОД\". Если какая-нибудь цифра из вашего числа совпала с цифрой из \r\nзадуманного числа и стоит на том же месте, то выведется сообщение \"БЫКИ: \r\n1\" , а, если такая цифра есть, но стоит на другом месте, то \"КОРОВЫ:1\".";
+
+            GroupBox groupBox = new GroupBox();
+            groupBox.Size = new Size(345, 420);
+            groupBox.Location = new Point(19, 31);
+            groupBox.Text = "О Программе";
+            groupBox.Name = "Programm";
+            Controls.Add(groupBox);
+
+            System.Windows.Forms.TextBox tbx = new System.Windows.Forms.TextBox();
+            tbx.Text = textReader;
+            tbx.BorderStyle = BorderStyle.FixedSingle;
+            tbx.Multiline = true;
+            tbx.Location = new Point(30, 40);
+            tbx.Size = new Size(285, 120);
+            tbx.ScrollBars = ScrollBars.Both;
+            Controls["Programm"].Controls.Add(tbx);
+
+            System.Windows.Forms.Button btn = new System.Windows.Forms.Button();
+            btn.Text = "Выход";
+            btn.Location = new Point(130, 180);
+            btn.FlatStyle = FlatStyle.Popup;
+            btn.Click += new EventHandler(btnClickCloseGrBox);
+            Controls["Programm"].Controls.Add(btn);
+
+
+            btnInput.Visible = false;
+            btnNewGame.Visible = false;
+            lbxBics.Visible = false;
+            lbxCorov.Visible = false;
+            lbxMainUser.Visible = false;
+            lblBics.Visible = false;
+            lblCorov.Visible = false;
+            lblUserNumber.Visible = false;
+            LblResult.Visible = false;
+            tbxInput.Visible = false;
+        }
+
+        private void btnClickCloseGrBox(object senber, EventArgs e) 
+        {
+            btnInput.Visible = true;
+            btnNewGame.Visible = true;
+            lbxBics.Visible = true;
+            lbxCorov.Visible = true;
+            lbxMainUser.Visible = true;
+            lblBics.Visible = true;
+            lblCorov.Visible = true;
+            lblUserNumber.Visible = true;
+            LblResult.Visible = true;
+            tbxInput.Visible = true;
+            /*Controls["Programm"].Controls.Clear();*/
+            Controls.RemoveByKey("Programm");
+        }
+
+        private void btnInput_Click(object sender, EventArgs e)
+        {
+            if (tbxInput.Text.Length == 4)
+            {
+                int[] userNumber = tbxInput.Text.Select(c => int.Parse(c.ToString())).ToArray();
+                int bulls = 0;
+                int cows = 0;
+
+                for (int i = 0; i < 4; i++)
+                {
+                    if (userNumber[i] == secretNumber[i])
+                    {
+                        bulls++;
+                    }
+                    else if (secretNumber.Contains(userNumber[i]))
+                    {
+                        cows++;
+                    }
+                }
+
+                lbxMainUser.Items.Add(string.Join("", userNumber));
+                lbxBics.Items.Add(bulls);
+                lbxCorov.Items.Add(cows);
+
+                if (bulls == 4)
+                {
+                    MessageBox.Show("Поздравляем! Вы угадали число!");
+                    lbxCorov.Items.Clear();
+                    lbxBics.Items.Clear();
+                    lbxMainUser.Items.Clear();
+                }
+
+                tbxInput.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Введите 4-х значное число!");
+            }
+        }
+
+    }
+}
